0359f496cb0149515d56266eea9da19714367658
janpeter <janpeter@bicat.com>
2015-04-16 17:20:50 +0200

    inlog via firebase

---
+++ app/home/home.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('myApp.home', ['ngRoute', 'firebase'])
+
+// Declared route
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/home', {
+			templateUrl: 'home/home.html',
+			controller: 'HomeCtrl'
+		});
+	}])
+
+// Home controller
+	.controller('HomeCtrl', ['$scope', '$firebaseSimpleLogin', function($scope, $firebaseSimpleLogin) {
+
+		$scope.SignIn = function(event) {
+			event.preventDefault();  // To prevent form refresh
+			var username = $scope.user.email;
+			var password = $scope.user.password;
+
+			loginObj.$login('password', {
+				email: username,
+				password: password
+			})
+				.then(function(user) {
+					// Success callback
+					console.log('Authentication successful');
+				}, function(error) {
+					// Failure callback
+					console.log('Authentication failure');
+				});
+		};
+
+		var firebaseObj = new Firebase("https://tammingajp.firebaseio.com");
+
+		var loginObj = $firebaseSimpleLogin(firebaseObj);
+
+	}]);
--- app/js/app.js
+++ app/js/app.js
@@ -4,13 +4,9 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'myApp.home'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
